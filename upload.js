d9313b99798088d89147bcd7e05891243ef05edd
Soledad Penades <sole@mozilla.com>
2014-01-28 16:22:35 +0000

    using process.spawn instead of process.exec for more buffering!

--- upload.js
+++ upload.js
@@ -1,25 +1,43 @@
-// ~/public_html/mortar_devtools/
-
 
 var options = require('./config');
 var username = options.username;
 var host = options.host;
 var remote_path = options.remote_path;
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var glob = require('glob');
 
 console.log('upload');
 console.log(options);
 
-
-// scp -R dist/ user@host:remote_path
-var scpOptions = ['-r', 'dist/*', username + '@' + host + ':' + remote_path ];
-
-var cmd = ['scp', scpOptions.join(' ')].join(' ');
-console.log('cmd', cmd);
-
-exec(cmd, function(error, stdout, stderr) {
-  console.log('error?', error);
-  console.log('stdout', stdout);
-  console.log('stderr', stderr);
+// When we use spawn we don't call a shell that does the wildcard expansion for us,
+// so we'll use glob to get the expanded list of files
+glob('dist/*', function(err, files) {
+  if(err) {
+    console.log(err);
+  } else {
+    uploadFiles(files);
+  }
 });
 
+// scp -R list of files user@host:remote_path
+function uploadFiles(files) {
+
+  var scpOptions = [ '-r' ].concat(files).concat(username + '@' + host + ':' + remote_path);
+
+  var scp = spawn('scp', scpOptions);
+
+  console.log('scp options', scpOptions);
+
+  scp.stdout.pipe(process.stdout);
+
+  scp.stderr.pipe(process.stderr);
+
+  scp.on('close', function(code, signal) {
+    if(code) {
+      console.error('Something went wrong; signal = ' + signal);
+    } else {
+      console.log('Child process terminated due to receipt of signal ' + signal);
+    }
+  });
+
+}
